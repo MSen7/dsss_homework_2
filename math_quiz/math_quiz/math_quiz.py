fffda833d6cb0ac4111bd1a58330b9355c3bd6d7
Maharshi Sen <maharshi.sen@fau.de>
2023-11-15 23:42:31 +0100

    math_quiz.py cleaned up

--- math_quiz/math_quiz/math_quiz.py
+++ math_quiz/math_quiz/math_quiz.py
@@ -1,46 +1,58 @@
 import random
 
-
-def function_A(min, max):
+#The function name from function_A has been changed to Generate_Random_Number
+def Generate_Random_Number(min, max):
     """
     Random integer.
     """
     return random.randint(min, max)
 
-
-def function_B():
+#The function name from function_B has been changed to Generate_Random_Arithmetic_Operator
+def Generate_Random_Arithmetic_Operator():
     return random.choice(['+', '-', '*'])
 
-
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+'''
+Variable names have been changed from n1,o,n2 to number1, operator,number2, 
+p has been changed to message and a to answer
+'''
+def Arithmetic_Operations(number1, number2, operator):
+    message = f"{number1} {operator} {number2}"
+    if operator == '+': answer = number1 + number2
+    elif operator == '-': answer = number1 - number2
+    else: answer = number1 * number2
+    return message,answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    '''s has been changed to score
+    and t_q to total trials'''
+    
+    score = 0
+    '''range() function cannot take a floating point value. So, changed it to 3.'''
+    total_trials = 3 
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_trials):
+        '''the random.randint() function cannot take a floating point value as min or max. So, changed it to 5.'''
+        n1 = Generate_Random_Number(1, 10); n2 = Generate_Random_Number(1, 5); o = Generate_Random_Arithmetic_Operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = Arithmetic_Operations(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        UserAnswer = input("Your answer: ")
+        try:
+            UserAnswer = int(UserAnswer)
+        except:
+            print('You must enter a number and not blank spaces or strings as inputs!')
+       
+            
 
-        if useranswer == ANSWER:
+        if UserAnswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
-
+    print(f"\nGame over! Your score is: {score/total_trials}")
 if __name__ == "__main__":
     math_quiz()
